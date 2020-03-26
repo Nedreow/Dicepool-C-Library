f442609ee85bdb30abfc4e4778878f61a59736d2
Nedreow <F.vanwoerden23@gmail.com>
2020-03-26 08:22:53 +0100

    Dice now return random results when rolled

--- DicePool/Die.cs
+++ DicePool/Die.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,7 +9,9 @@ namespace DicePool
         public string Name { get; }
 
         public List<string> Facets { get; }
-        
+
+        private Random random;
+
         public Die(int sides, string name = "")
         {
             if (name == "")
@@ -21,17 +24,19 @@ namespace DicePool
             }
             
             Facets = Enumerable.Range(1, sides).Select(x => x.ToString()).ToList();
+            random = new Random();
         }
 
         public Die(List<string> facets, string name = "")
         {
             Name = name;
             Facets = facets;
+            random = new Random();
         }
 
         public string Roll()
         {
-            return Facets[0];
+            return Facets[random.Next(1, Facets.Count + 1)];
         }
     }
 }
