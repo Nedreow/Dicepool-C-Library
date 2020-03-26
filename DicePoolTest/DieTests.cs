43092b6d5fe2417ba8f942b14420f33d6808904b
Nedreow <F.vanwoerden23@gmail.com>
2020-03-26 08:41:30 +0100

    Add tests

--- DicePoolTest/DieTests.cs
+++ DicePoolTest/DieTests.cs
@@ -15,5 +15,31 @@ namespace DicePoolTest
             var expectedFacets = new List<string>{"1", "2", "3", "4", "5", "6"};
             Assert.AreEqual(expectedFacets, testDie.Facets);
         }
+
+        [Test]
+        public void CreateStringDie()
+        {
+            var testFacets = new List<string>() {"result1", "result2", "result3", "result4"};
+            var testDie = new Die(testFacets, "Test Name");
+            
+            Assert.AreEqual("Test Name", testDie.Name);
+            Assert.AreEqual(testFacets, testDie.Facets);
+        }
+
+        [Test]
+        public void TestRoll()
+        {
+            var testDie = CreateTestNumericDie(6);
+            var expectedResults = new List<string> {"1", "2", "3", "4", "5", "6"};
+
+            var rollResult = testDie.Roll();
+            
+            Assert.IsTrue(expectedResults.Contains(rollResult));
+        }
+
+        private Die CreateTestNumericDie(int sides, string name = "")
+        {
+            return new Die(sides, name);
+        }
     }
 }
